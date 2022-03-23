64e5366ff5919cdcf5e321e354e5dc77f576a192
Burak Keçeci <bburakkececi@gmail.com>
2022-03-23 15:19:24 +0300

    uploaded

---
+++ numberfinder.py
@@ -0,0 +1,15 @@
+import random
+random.seed()
+randomNumber = random.randrange(0,100)
+i,number = 1, -1
+while number != randomNumber:
+    number = int(input("Enter the number between 0-100: "))
+    if(number < randomNumber):
+        print("Up")
+    elif (number > randomNumber):
+        print("Down")
+    else:
+        print("Congratulations! Got it on the {}th try. ".format(i))
+        break
+    i = i + 1
+
